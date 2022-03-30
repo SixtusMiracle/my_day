25dfc24f35b5392c482644754450453939c05572
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-03-30 09:26:48 +0100

    displaying dynamic todos

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_day/models/todo.dart';
 import 'package:my_day/shared/constants.dart';
-import 'package:intl/intl.dart';
 
 import '../../../shared/util/background.dart';
-import '../../../shared/util/circular_gradient_icon.dart';
-import '../../../shared/util/color_dot.dart';
+import 'todo_card.dart';
 
 class Body extends StatelessWidget {
   const Body({
@@ -22,65 +19,12 @@ class Body extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: miDefaultSize * 2.3),
-            Card(
-              // TODO: Extract this widget
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(miDefaultSize),
-              ),
-              elevation: 3.0,
-              child: ListTile(
-                onTap: () {},
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: miDefaultSize * 0.8,
-                  horizontal: miDefaultSize * 0.6,
-                ),
-                leading: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: ColorDot(color: Color(todos[0].colorCode)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: miDefaultSize * 0.4),
-                      child: CircularGradientIcon(
-                        icon: todos[0].icon,
-                        gradientStartColor: todos[0].gradientStartColor,
-                        gradientStopColor: todos[0].gradientStopColor,
-                        beginStart: todos[0].beginStart,
-                        beginStop: todos[0].beginStop,
-                        endStart: todos[0].endStart,
-                        endStop: todos[0].endStop,
-                      ),
-                    ),
-                  ],
-                ),
-                title: Text(
-                  todos[0].title,
-                  style: TextStyle(
-                    fontSize: miDefaultSize * 1.20,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                trailing: Column(
-                  children: [
-                    Text(
-                      DateFormat('d MMM').format(todos[0].schedule),
-                      style: TextStyle(
-                        fontFamily: "Lato Black",
-                        fontSize: miDefaultSize * 1.2,
-                      ),
-                    ),
-                    SizedBox(height: miDefaultSize - 4),
-                    Text(
-                      DateFormat.Hm().format(todos[0].schedule),
-                      style: TextStyle(
-                        fontFamily: "Lato Light",
-                      ),
-                    ),
-                  ],
-                ),
+            Expanded(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemCount: todos.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    TodoCard(todo: todos[index], press: () {}),
               ),
             ),
           ],
---
+++ lib/screens/home/components/todo_card.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../models/todo.dart';
+import '../../../shared/constants.dart';
+import '../../../shared/util/circular_gradient_icon.dart';
+import '../../../shared/util/color_dot.dart';
+
+class TodoCard extends StatelessWidget {
+  final Todo todo;
+  final Function()? press;
+
+  const TodoCard({
+    Key? key,
+    required this.todo,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Card(
+        margin: EdgeInsets.only(bottom: 20.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(miDefaultSize),
+        ),
+        elevation: 3.0,
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: miDefaultSize * 0.8,
+            horizontal: miDefaultSize * 0.6,
+          ),
+          leading: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                child: ColorDot(color: Color(todo.colorCode)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: miDefaultSize * 0.4),
+                child: CircularGradientIcon(
+                  icon: todo.icon,
+                  gradientStartColor: todo.gradientStartColor,
+                  gradientStopColor: todo.gradientStopColor,
+                  beginStart: todo.beginStart,
+                  beginStop: todo.beginStop,
+                  endStart: todo.endStart,
+                  endStop: todo.endStop,
+                ),
+              ),
+            ],
+          ),
+          title: Text(
+            todo.title,
+            style: TextStyle(
+              fontSize: miDefaultSize * 1.20,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          trailing: Column(
+            children: [
+              Text(
+                DateFormat('d MMM').format(todo.schedule),
+                style: TextStyle(
+                  fontFamily: "Lato Black",
+                  fontSize: miDefaultSize * 1.2,
+                ),
+              ),
+              SizedBox(height: miDefaultSize - 4),
+              Text(
+                DateFormat.Hm().format(todo.schedule),
+                style: TextStyle(
+                  fontFamily: "Lato Light",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
